df6887cf8648e55999803c128f2a67d82b5664fc
cissty <128071188+cissty@users.noreply.github.com>
2023-04-16 20:07:12 +0300

    added a slider and tracking value

--- script.js
+++ script.js
@@ -2,5 +2,16 @@
 // be able to draw something on it
 // user can choose the color
 // will be a reset button to remove everything but keep the grids.
-//
 const grid = document.getElementById('container');
+
+const sliderEl = document.querySelector("#range")
+const sliderValue = document.querySelector(".value")
+
+sliderEl.addEventListener("input", e => {
+  const tempSliderValue = e.target.value;
+  sliderValue.textContent = `${tempSliderValue} x ${tempSliderValue}`
+
+
+})
+
+
