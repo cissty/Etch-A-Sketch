2a5b76aae0b7bb0125b44c459306adc59370171b
cissty <128071188+cissty@users.noreply.github.com>
2023-04-16 06:47:26 +0300

    added css, js, html

---
+++ script.js
@@ -0,0 +1,8 @@
+// a custom section or prompt ask the user how many grids.
+// be able to draw something on it
+// user can choose the color
+// will be a reset button to remove everything but keep the grids.
+//
+const grid = document.getElementById('container');
+
+grid.setAttribute('style', 'border: 10px solid red');
