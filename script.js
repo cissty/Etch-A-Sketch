998da4cf912b1c7c69c54338f2b8b2815abee981
cissty <128071188+cissty@users.noreply.github.com>
2023-04-17 15:54:24 +0300

    fix the bug

--- script.js
+++ script.js
@@ -9,51 +9,56 @@
 const sliderElement = document.querySelector("#range");
 const sliderValue = document.querySelector(".value");
 const grid = document.getElementById('container');
+let tempSliderValue = sliderElement.value;
 
-let tempSliderValue;
-
-function forSlider(event){
-  tempSliderValue = event.target.value;
+function forSlider(){
+  tempSliderValue = sliderElement.value;
   sliderValue.textContent = `${tempSliderValue} x ${tempSliderValue}`;
   // Assigning the increased or decreased value to the columns and rows
   const columns = `repeat(${tempSliderValue}, 1fr)`
   const rows = `repeat(${tempSliderValue}, 1fr)`
   grid.style.gridTemplateColumns = columns;
   grid.style.gridTemplateRows = rows; 
-  divs();
-
-
 }
 
-  sliderElement.addEventListener("input", forSlider);
+let newDivs = document.createElement('div');
 
-//creating new div//
-function divs() {
+  //creating new div//
+function createDivs() {
   
-  for(let i = 0; i < tempSliderValue * 2; i++){
-    let newDivs = document.createElement('div');
-    newDivs.classList.add('newDiv');
-    grid.appendChild(newDivs);
+    for (let i = 0; i < tempSliderValue * tempSliderValue; i++){
+      newDivs = document.createElement('div');
 
+      newDivs.classList.add('newDiv');
+      grid.appendChild(newDivs);
+    };
+}
 
- // Add mousedown and mouseover event listeners to each div
-    newDivs.addEventListener('mousedown', () =>{
-      newDivs.style.backgroundColor = 'black';
-      newDivs.addEventListener('mouseover', () => {
-        newDivs.style.backgroundColor = 'blue';
-      });
-    });
-    }
-  };
+////
+  sliderElement.addEventListener("input", function() {
+    forSlider();
+    createDivs();
+  });
+  
 
-// Adding a reset button --clear the box-- //
+// Add mousedown and mouseover event listeners to each div
+    
+ 
+
+//    Adding a reset button --clear the box-- //
 
      const reset = document.getElementById('clear');
-    
+     const allDivs = document.querySelectorAll('.newDiv');
      reset.addEventListener('click', () => {
-    const allDivs = document.querySelectorAll('.newDiv');
-    allDivs.forEach((div) => {
+     allDivs.forEach((div) => {
       div.style.backgroundColor = '';
   });
 
+});
+
+allDivs.addEventListener('mousedown', () =>{
+  allDivs.style.backgroundColor = 'black';
+  allDivs.addEventListener('mouseover', () => {
+    allDivs.style.backgroundColor = 'blue';
+  });
 });
