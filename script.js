8440a3eae550d9365ddd9fa7d7fe696abe2e9172
cissty <128071188+cissty@users.noreply.github.com>
2023-04-18 07:26:03 +0300

    javascript updated, some adjustment to css and html

--- script.js
+++ script.js
@@ -10,55 +10,92 @@ const sliderElement = document.querySelector("#range");
 const sliderValue = document.querySelector(".value");
 const grid = document.getElementById('container');
 let tempSliderValue = sliderElement.value;
+let gridColumns = tempSliderValue;
+let gridRows = gridColumns;
 
 function forSlider(){
   tempSliderValue = sliderElement.value;
   sliderValue.textContent = `${tempSliderValue} x ${tempSliderValue}`;
   // Assigning the increased or decreased value to the columns and rows
-  const columns = `repeat(${tempSliderValue}, 1fr)`
-  const rows = `repeat(${tempSliderValue}, 1fr)`
+   columns = `repeat(${tempSliderValue}, 1fr)`
+   rows = `repeat(${tempSliderValue}, 1fr)`
   grid.style.gridTemplateColumns = columns;
   grid.style.gridTemplateRows = rows; 
 }
 
-let newDivs = document.createElement('div');
 
-  //creating new div//
-function createDivs() {
-  
-    for (let i = 0; i < tempSliderValue * tempSliderValue; i++){
-      newDivs = document.createElement('div');
 
-      newDivs.classList.add('newDiv');
-      grid.appendChild(newDivs);
-    };
-}
 
-////
-  sliderElement.addEventListener("input", function() {
+////removing old div when the value is changed.
+  sliderElement.addEventListener("input", () =>{
     forSlider();
-    createDivs();
+    while(grid.firstChild){
+     grid.removeChild(grid.firstChild);
+    }
+
+    //Create the new grid with loop
+    for (let row = 0; row < tempSliderValue; row++){
+      for (let column = 0; column < tempSliderValue; column++){
+        const gridItem = document.createElement('div');
+        gridItem.classList.add('newDiv');
+        grid.appendChild(gridItem);
+    
+    // Adding adjustable color //
+
+
+
+// Add Color mousedown and mouseover event listeners to each div
+     const colorMode = document.getElementById('colorMode');
+
+     colorMode.addEventListener('click', () => {
+      var color = document.getElementById('myColor');
+      gridItem.addEventListener('mousedown', () => {
+        gridItem.style.backgroundColor = `${color.value}`;
+      });
+  
+      gridItem.addEventListener('mouseover', (event) => {
+        if(event.buttons == 1){
+            gridItem.style.backgroundColor = `${color.value}`;
+        }
+      });
+     });
+
+// Add Eraser for mousedown and mouseover event listeners to each div
+    const eraser = document.getElementById('eraser');
+
+     eraser.addEventListener('click', () => {
+      gridItem.addEventListener('mousedown', () => {
+        gridItem.style.backgroundColor = 'white';
+      });
+  
+      gridItem.addEventListener('mouseover', (event) => {
+        if(event.buttons == 1){
+            gridItem.style.backgroundColor = 'white';
+        }
+      });
+     });
+      //----
+      }
+    }
   });
   
 
-// Add mousedown and mouseover event listeners to each div
+   
+
+
+
     
  
 
 //    Adding a reset button --clear the box-- //
 
      const reset = document.getElementById('clear');
-     const allDivs = document.querySelectorAll('.newDiv');
+
      reset.addEventListener('click', () => {
-     allDivs.forEach((div) => {
-      div.style.backgroundColor = '';
-  });
+     const allDiv = document.querySelectorAll('.newDiv');
+      allDiv.forEach(div => {
+        div.style.backgroundColor = '';
 
-});
+      });
+    });
 
-allDivs.addEventListener('mousedown', () =>{
-  allDivs.style.backgroundColor = 'black';
-  allDivs.addEventListener('mouseover', () => {
-    allDivs.style.backgroundColor = 'blue';
-  });
-});
